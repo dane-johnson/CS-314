157f0bd422ef9c9b4b880a4691eb4856cb5231c5
dane-johnson <dane_johnson@ymail.com>
2017-05-12 15:30:46 -0500

    added project3

---
+++ project3/src/userprog2.c
@@ -0,0 +1,11 @@
+#include "lab2-api.h"
+
+//----------------------------------------------------------------------------
+//   You are free to modify this file (and other userprogs) to test your solution
+//   Understand the scheduling algorithm well before you hack or you'll get nowhere. 
+//----------------------------------------------------------------------------
+main (int argc, char *argv[])
+{
+  Printf("Test\n");
+  process_create(0, 0, "userprog2.dlx.obj", 0, 0, NULL);
+}
